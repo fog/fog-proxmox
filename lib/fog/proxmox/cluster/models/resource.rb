614b60bcf96ac48f953853b0cb53b206335fd222
Yvan E. Watchman <git@yvanwatchman.eu>
2022-10-27 01:42:30 +0200

    add initial code for endpoint/request.

---
+++ lib/fog/proxmox/cluster/models/resource.rb
@@ -0,0 +1,57 @@
+# Copyright 2018 Tristan Robert
+
+# This file is part of Fog::Proxmox.
+
+# Fog::Proxmox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Fog::Proxmox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Fog::Proxmox. If not, see <http://www.gnu.org/licenses/>.
+
+# frozen_string_literal: true
+
+module Fog
+  module Proxmox
+    class Cluster
+      # class Disk model
+      class Resource < Fog::Model
+        identity  :id
+
+        attribute :name
+        attribute :type
+        attribute :node
+
+        # generic
+        attribute :maxdisk
+
+        # vm/lxc specific
+        attribute :status
+        attribute :uptime
+        attribute :template
+        # resources are written in bytes, proxmox displays gibibytes
+        attribute :maxcpu
+        attribute :maxmem
+
+        # storage specific
+        attribute :content
+        attribute :shared
+        attribute :storage
+
+        def is_template?
+          template === 1
+        end
+
+        def to_s
+          Fog::Proxmox::Hash.flatten(flatten)
+        end
+      end
+    end
+  end
+end
---
+++ lib/fog/proxmox/cluster/models/resources.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Copyright 2018 Tristan Robert
+
+# This file is part of Fog::Proxmox.
+
+# Fog::Proxmox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Fog::Proxmox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Fog::Proxmox. If not, see <http://www.gnu.org/licenses/>.
+
+require 'fog/proxmox/cluster/models/resource'
+
+module Fog
+  module Proxmox
+    class Cluster
+      # class Disks Collection of disk
+      class Resources < Fog::Collection
+        model Fog::Proxmox::Cluster::Resource
+
+        def all
+          self
+        end
+
+        def get(id)
+          all.find { |resource| resource.identity === id }
+        end
+      end
+    end
+  end
+end
